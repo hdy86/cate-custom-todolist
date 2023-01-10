e44ca3a5ed5a4ce334ac244498b347fb19273b77
hdy86 <dyhong86@naver.com>
2023-01-10 21:01:17 +0900

    category 나누기

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,7 +1,43 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text }: IToDo) {
-  return <li>{text}</li>;
+function ToDo({ id, text, category }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = e;
+    setToDos((state) => {
+      const targetIdx = state.findIndex((item) => item.id === id);
+      const newToDo = { id, text, category: name as any };
+      return [
+        ...state.slice(0, targetIdx),
+        newToDo,
+        ...state.slice(targetIdx + 1),
+      ];
+    });
+  };
+
+  return (
+    <li>
+      <span>{text}</span>
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
+    </li>
+  );
 }
 
 export default ToDo;
