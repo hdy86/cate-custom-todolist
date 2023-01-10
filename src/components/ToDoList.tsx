8530890a688cf6070b1eee190c55b9c079d22ef1
hdy86 <dyhong86@naver.com>
2023-01-10 21:24:20 +0900

    enum 으로 수정

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,5 +1,5 @@
 import { useRecoilState, useRecoilValue } from "recoil";
-import { toDoState, toDoSelector, categoryState } from "../atoms";
+import { toDoState, toDoSelector, categoryState, Categories } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
@@ -7,7 +7,7 @@ function ToDoList() {
   const toDos = useRecoilValue(toDoSelector);
   const [category, setCategory] = useRecoilState(categoryState);
   const onInput = (e: React.FormEvent<HTMLSelectElement>) => {
-    setCategory(e.currentTarget.value);
+    setCategory(e.currentTarget.value as any);
   };
 
   return (
@@ -16,9 +16,9 @@ function ToDoList() {
       <hr />
       <CreateToDo />
       <select value={category} onInput={onInput}>
-        <option value="TO_DO">To Do</option>
-        <option value="DOING">Doing</option>
-        <option value="DONE">Done</option>
+        <option value={Categories.TO_DO}>To Do</option>
+        <option value={Categories.DOING}>Doing</option>
+        <option value={Categories.DONE}>Done</option>
       </select>
       {toDos?.map((toDo) => (
         <ToDo key={toDo.id} {...toDo} />
